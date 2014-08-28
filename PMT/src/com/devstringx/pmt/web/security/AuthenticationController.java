0bd7695f0531ba2517d024570482cfd07a8008a1
rajeevshukla <mail2rajeevshukla@gmail.com>
2014-08-28 22:36:24 +0530

    Velocity template and authentication contoller has been added.

--- PMT/src/com/devstringx/pmt/web/security/AuthenticationController.java
+++ PMT/src/com/devstringx/pmt/web/security/AuthenticationController.java
@@ -2,12 +2,28 @@ package com.devstringx.pmt.web.security;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AuthenticationController {
 	
 	
+	@RequestMapping(value="login.htm")
+	public ModelAndView getLoginPage(){
+		ModelAndView  mav=new  ModelAndView("security/login");
+		
+		
+		return mav;
+	}
 	
+	
+	@RequestMapping(value="getRegisterPage.htm")
+	public ModelAndView getRegisterPage(){
+		ModelAndView  mav=new  ModelAndView("security/registerPage");
+	
+		
+		return mav;
+	}
 	
 
 }
